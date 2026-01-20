1374f8ce812a948a4a8c520582c776bf6317badc
Nikita Mitasov <me@ch4og.com>
2026-01-20 21:03:22 +0300

    packages: protonplus: init

---
+++ shika/packages/protonplus.scm
@@ -0,0 +1,71 @@
+;;; SPDX-FileCopyrightText: 2025-2026 Nikita Mitasov <me@ch4og.com>
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+
+(define-module (shika packages protonplus)
+  #:use-module (guix git-download)
+  #:use-module (guix gexp)
+  #:use-module (guix packages)
+  #:use-module (guix build-system meson)
+  #:use-module (gnu packages backup)
+  #:use-module (gnu packages freedesktop)
+  #:use-module (gnu packages gettext)
+  #:use-module (gnu packages glib)
+  #:use-module (gnu packages gnome)
+  #:use-module (gnu packages nss)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages tls)
+  #:use-module ((guix licenses) #:prefix license:))
+
+(define-public protonplus
+  (package
+    (name "protonplus")
+    (version "0.5.15")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+              (url "https://github.com/Vysp3r/protonplus")
+              (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "09mbkdy49rlikwh1nz8a5215sl3g85gxanykcinfmjnalqa0ynjl"))))
+    (build-system meson-build-system)
+    (arguments (list #:glib-or-gtk? #t))
+    (native-inputs (list gettext-minimal
+                         `(,glib "bin")
+                         pkg-config
+                         vala))
+    (inputs (list desktop-file-utils
+                  gsettings-desktop-schemas
+                  `(,gtk "bin")
+                  json-glib
+                  libadwaita
+                  libarchive
+                  libgee
+                  libsoup))
+    (home-page "https://github.com/Vysp3r/protonplus")
+    (synopsis "Simple Wine and Proton-based compatibility tools manager.")
+    (description
+     "ProtonPlus is a Proton version manager for installing and managing Proton versions.
+It works with Steam, Lutris, Heroic Games Launcher and Bottles. It uses GTK4.")
+    (license license:gpl3)))
+
+(define-public protonplus-sandbox
+  (package
+    (inherit protonplus)
+    (name "protonplus-sandbox")
+    (arguments (list #:glib-or-gtk? #t
+                     #:phases
+                     #~(modify-phases %standard-phases
+                         (add-after 'install 'set-home
+                           (lambda _
+                             (let* ((bin (string-append #$output "/bin/protonplus"))
+                                    (new-home "$HOME/.local/share/guix-sandbox-home/"))
+                               (wrap-program bin
+                                 `("HOME" =
+                                   (,(string-append "${GUIX_SANDBOX_HOME:-" new-home "}"))))))))))
+    (synopsis "Simple Wine and Proton-based compatibility tools manager.
+Patched for nonguix container path.")))
+
+protonplus
