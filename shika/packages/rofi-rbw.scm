20cceaa13ec9ca49304d6d30a2228a2966caceb8
Nikita Mitasov <me@ch4og.com>
2026-02-07 13:53:15 +0300

    packages: rofi-rbw: init at 1.5.1

---
+++ shika/packages/rofi-rbw.scm
@@ -0,0 +1,33 @@
+;;; SPDX-FileCopyrightText: 2026 Nikita Mitasov <me@ch4og.com>
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+
+(define-module (shika packages rofi-rbw)
+  #:use-module (guix download)
+  #:use-module (guix packages)
+  #:use-module (guix build-system pyproject)
+  #:use-module (guix build-system python)
+  #:use-module (gnu packages python-build)
+  #:use-module (gnu packages python-xyz)
+  #:use-module ((guix licenses) #:prefix license:))
+
+(define-public rofi-rbw
+  (package
+    (name "rofi-rbw")
+    (version "1.5.1")
+    (source (origin
+              (method url-fetch)
+              (uri (pypi-uri "rofi_rbw" version))
+              (sha256
+               (base32
+                "06xc1l791mc18gddq4rbf13j1s24xv4jj8570mjxcknbjrfn59y6"))))
+    (build-system pyproject-build-system)
+    (native-inputs (list python-hatchling))
+    (inputs (list python-configargparse))
+    (home-page "https://github.com/fdw/rofi-rbw")
+    (synopsis "Rofi frontend for Bitwarden")
+    (description
+     "Based on the alternative Bitwarden CLI rbw and inspired by rofi-pass,
+rbw-rofi is a simplistic password typer/copier using rofi, wofi, fuzzel, and bemenu.")
+    (license license:expat)))
+
+rofi-rbw
