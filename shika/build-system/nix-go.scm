41462964b7da6fc0abca5612ee57d2de81cd59de
Nikita Mitasov <me@ch4og.com>
2026-02-09 21:05:51 +0300

    build: style

--- shika/build-system/nix-go.scm
+++ shika/build-system/nix-go.scm
@@ -25,8 +25,8 @@
 
 (define %nix-go-build-system-modules
   `((guix build utils)
-    ;;(guix gexp)
-    ;;(guix build-system)
+    ;; (guix gexp)
+    ;; (guix build-system)
     (shika build nix-go-build-system)
     ,@%default-gnu-imported-modules))
 
@@ -38,12 +38,21 @@
 (define-record-type* <go-vendor-config> go-vendor-config
   make-go-vendor-config
   go-vendor-config?
-  (url go-vendor-config-url (default #f))
-  (toolchain go-vendor-config-toolchain (default #f))
-  (tidy? go-vendor-config-tidy? (default #t)))
+  (url go-vendor-config-url
+       (default #f))
+  (toolchain go-vendor-config-toolchain
+             (default #f))
+  (tidy? go-vendor-config-tidy?
+         (default #t)))
 
 (define* (lower name
-                #:key source inputs native-inputs outputs system target
+                #:key
+                source
+                inputs
+                native-inputs
+                outputs
+                system
+                target
                 (build-flags '())
                 (go #f)
                 (tidy? #t)
@@ -56,70 +65,79 @@
   (define private-keywords
     '(#:target #:go #:inputs #:native-inputs #:vendor-hash #:tidy?))
 
-  (define* (go-vendor-fetch cfg hash-algo hash name #:key system)
+  (define* (go-vendor-fetch cfg
+                            hash-algo
+                            hash
+                            name
+                            #:key system)
     (let ((pkg (go-vendor-config-url cfg))
           (go-toolchain (go-vendor-config-toolchain cfg))
           (tidy? (go-vendor-config-tidy? cfg)))
-      (gexp->derivation
-       (string-append name "-vendor")
-       #~(begin
-           (let ((tmpdir (or (getenv "TMPDIR") "/tmp")))
-             (use-modules (guix build utils)
-                          (guix search-paths))
+      (gexp->derivation (string-append name "-vendor")
+                        #~(begin
+                            (let ((tmpdir (or (getenv "TMPDIR") "/tmp")))
+                              (use-modules (guix build utils)
+                                           (guix search-paths))
 
-             (setenv "PATH" (cdar (evaluate-search-paths (list $PATH)
-                                                         (list
-                                                          #$git-minimal #$go-toolchain
-                                                          #$@(map cadr (standard-packages))))))
+                              (setenv "PATH"
+                                      (cdar (evaluate-search-paths (list $PATH)
+                                                                   (list #$git-minimal
+                                                                         #$go-toolchain
+                                                                         #$@(map
+                                                                             cadr
+                                                                             (standard-packages))))))
 
-             (setenv "SSL_CERT_DIR" (string-append #$nss-certs "/etc/ssl/certs"))
-             (setenv "GOPATH" (string-append tmpdir "/go"))
-             (setenv "GOCACHE" (string-append tmpdir "/go-cache"))
-             (setenv "GOPROXY" "https://proxy.golang.org|direct")
-             (setenv "GOTELEMETRY" "off")
-             (setenv "GO111MODULE" "on")
-             (setenv "GOTOOLCHAIN" "local")
-             (setenv "CGO_ENABLED" "0")
+                              (setenv "SSL_CERT_DIR" (string-append #$nss-certs "/etc/ssl/certs"))
+                              (setenv "GOPATH" (string-append tmpdir "/go"))
+                              (setenv "GOCACHE" (string-append tmpdir "/go-cache"))
+                              (setenv "GOPROXY" "https://proxy.golang.org|direct")
+                              (setenv "GOTELEMETRY" "off")
+                              (setenv "GO111MODULE" "on")
+                              (setenv "GOTOOLCHAIN" "local")
+                              (setenv "CGO_ENABLED" "0")
 
-             (let ((st (stat #$source)))
-               (if (eq? (stat:type st) 'directory)
-                   (invoke "cp" "-r" "--reflink=auto" #$source "modRoot")
-                   (begin
-                     (mkdir-p "modRoot")
-                     (invoke "tar" "zxvf" #$source "--strip-components=1" "-C" "modRoot"))))
+                              (let ((st (stat #$source)))
+                                (if (eq? (stat:type st) 'directory)
+                                    (invoke "cp" "-r" "--reflink=auto" #$source "modRoot")
+                                    (begin
+                                      (mkdir-p "modRoot")
+                                      (invoke "tar" "zxvf" #$source "--strip-components=1" "-C" "modRoot"))))
 
-             (chdir "modRoot")
-             (chmod "." #o755)
-             (chmod "go.mod" #o644)
-             (chmod "go.sum" #o644)
-             (invoke "chmod" "-R" "u+w" ".")
+                              (chdir "modRoot")
+                              (chmod "." #o755)
+                              (chmod "go.mod" #o644)
+                              (chmod "go.sum" #o644)
+                              (invoke "chmod" "-R" "u+w" ".")
 
-             (when #$tidy?
-               (invoke "go" "mod" "tidy"))
-             (invoke "go" "mod" "vendor")
-             (mkdir-p "vendor")
-             (mkdir-p #$output)
+                              (when #$tidy?
+                                (invoke "go" "mod" "tidy"))
+                              (invoke "go" "mod" "vendor")
+                              (mkdir-p "vendor")
+                              (mkdir-p #$output)
 
-             (invoke "cp" "-r" "--reflink=auto" "go.mod" "go.sum" "vendor"
-                     #$output)))
+                              (invoke "cp" "-r" "--reflink=auto" "go.mod" "go.sum" "vendor"
+                                      #$output)))
 
-       #:modules
-       '((guix build utils)
-         (guix records)
-         (guix search-paths))
-       #:local-build? #t
-       #:recursive? #t
-       #:system system
-       #:hash-algo hash-algo
-       #:hash hash
-       #:leaked-env-vars '("http_proxy" "https_proxy" "LC_ALL" "LC_MESSAGES" "LANG" "COLUMNS"))))
+                        #:modules '((guix build utils)
+                                    (guix records)
+                                    (guix search-paths))
+                        #:local-build? #t
+                        #:recursive? #t
+                        #:system system
+                        #:hash-algo hash-algo
+                        #:hash hash
+                        #:leaked-env-vars '("http_proxy"
+                                            "https_proxy"
+                                            "LC_ALL"
+                                            "LC_MESSAGES"
+                                            "LANG"
+                                            "COLUMNS"))))
 
   (define (vendor-origin name vendor-hash go-toolchain tidy?)
     (pkgs:origin (method go-vendor-fetch)
-                 (uri (go-vendor-config
-                       (url source)
-                       (toolchain go-toolchain)
-                       (tidy? tidy?)))
+                 (uri (go-vendor-config (url source)
+                                        (toolchain go-toolchain)
+                                        (tidy? tidy?)))
                  (file-name name)
                  (hash (pkgs:content-hash (nix-base32-string->bytevector vendor-hash) sha256))))
 
@@ -128,43 +146,46 @@
         go
         (default-go)))
 
-  (bag
-    (name name)
-    (system system)
-    (target target)
-    (build-inputs `(,@(if source
-                          `(("source" ,source))
-                          '())
-                    ,@(if vendor-hash
-                          `(("vendor" ,(vendor-origin name vendor-hash go-toolchain tidy?)))
-                          '())
-                    ,@`(("go" ,go-toolchain))
-                    ,@native-inputs
-                    ,@inputs
-                    ,@(if target
-                          ;; Use the standard cross inputs of
-                          ;; 'gnu-build-system'.
-                          (standard-cross-packages target 'host)
-                          '())
-                    ;; Keep the standard inputs of 'gnu-build-system'.
-                    ,@(standard-packages)))
-    (host-inputs '())
+  (bag (name name)
+       (system system)
+       (target target)
+       (build-inputs `(,@(if source
+                             `(("source" ,source))
+                             '()) ,@(if vendor-hash
+                                        `(("vendor" ,(vendor-origin name
+                                                                    vendor-hash
+                                                                    go-toolchain
+                                                                    tidy?)))
+                                        '())
+                             ,@`(("go" ,go-toolchain))
+                             ,@native-inputs
+                             ,@inputs
+                             ,@(if target
+                                   ;; Use the standard cross inputs of
+                                   ;; 'gnu-build-system'.
+                                   (standard-cross-packages target 'host)
+                                   '())
+                             ;; Keep the standard inputs of 'gnu-build-system'.
+                             ,@(standard-packages)))
+       (host-inputs '())
 
-    ;; The cross-libc is really a target package, but for bootstrapping
-    ;; reasons, we can't put it in 'host-inputs'.  Namely, 'cross-gcc' is a
-    ;; native package, so it would end up using a "native" variant of
-    ;; 'cross-libc' (built with 'gnu-build'), whereas all the other packages
-    ;; would use a target variant (built with 'gnu-cross-build'.)
-    (target-inputs (if target
-                       (standard-cross-packages target 'target)
-                       '()))
+       ;; The cross-libc is really a target package, but for bootstrapping
+       ;; reasons, we can't put it in 'host-inputs'.  Namely, 'cross-gcc' is a
+       ;; native package, so it would end up using a "native" variant of
+       ;; 'cross-libc' (built with 'gnu-build'), whereas all the other packages
+       ;; would use a target variant (built with 'gnu-cross-build'.)
+       (target-inputs (if target
+                          (standard-cross-packages target
+                                                   'target)
+                          '()))
 
-    (outputs outputs)
-    (build nix-go-build)
-    (arguments (cons* #:build-flags build-flags
-                      (strip-keyword-arguments private-keywords arguments)))))
+       (outputs outputs)
+       (build nix-go-build)
+       (arguments (cons* #:build-flags build-flags
+                         (strip-keyword-arguments private-keywords arguments)))))
 
-(define* (nix-go-build name inputs
+(define* (nix-go-build name
+                       inputs
                        #:key
                        source
                        (phases '%standard-phases)
@@ -188,49 +209,48 @@
                                   (guix build utils)))
 
                        (substitutable? #t))
-
+  
   (define builder
     (with-imported-modules imported-modules
       #~(begin
-	  (use-modules #$@(sexp->gexp modules))
+          (use-modules #$@(sexp->gexp modules))
 
-	  (define %outputs
-	    #$(outputs->gexp outputs))
+          (define %outputs
+            #$(outputs->gexp outputs))
 
-	  (nix-go-build #:name #$name
-			#:source #+source
-			#:system #$system
-			#:phases #$phases
-			#:outputs #$(outputs->gexp outputs)
-			#:substitutable? #$substitutable?
-			#:goarch #$goarch
-			#:goos #$goos
-			#:ldflags #$ldflags
-			#:tags #$tags
-			#:sub-packages (list #$@sub-packages)
-			#:search-paths '#$(sexp->gexp
-					   (map search-path-specification->sexp
-						search-paths))
-			#:install-source? #$install-source?
-			#:build-flags (list #$@build-flags)
-			#:tests? #$tests?
-			#:parallel-build? #$parallel-build?
-			#:parallel-tests? #$parallel-tests?
-			#:allow-go-reference? #$allow-go-reference?
-			#:inputs #$(input-tuples->gexp inputs)))))
+          (nix-go-build #:name #$name
+                        #:source #+source
+                        #:system #$system
+                        #:phases #$phases
+                        #:outputs #$(outputs->gexp outputs)
+                        #:substitutable? #$substitutable?
+                        #:goarch #$goarch
+                        #:goos #$goos
+                        #:ldflags #$ldflags
+                        #:tags #$tags
+                        #:sub-packages (list #$@sub-packages)
+                        #:search-paths '#$(sexp->gexp (map
+                                                       search-path-specification->sexp
+                                                       search-paths))
+                        #:install-source? #$install-source?
+                        #:build-flags (list #$@build-flags)
+                        #:tests? #$tests?
+                        #:parallel-build? #$parallel-build?
+                        #:parallel-tests? #$parallel-tests?
+                        #:allow-go-reference? #$allow-go-reference?
+                        #:inputs #$(input-tuples->gexp inputs)))))
 
-  (mlet %store-monad ((guile (pkgs:package->derivation (or guile (pkgs:default-guile))
-                                                       system #:graft? #f)))
-    (gexp->derivation name builder
-		      #:system system
-		      #:guile-for-build guile)))
+  (mlet %store-monad
+      ((guile (pkgs:package->derivation (or guile
+                                            (pkgs:default-guile)) system
+                                            #:graft? #f)))
+    (gexp->derivation name
+                      builder
+                      #:system system
+                      #:guile-for-build guile)))
 
 (define nix-go-build-system
-  (build-system
-    (name 'nix-go)
-    (description
-     "Build system for Go programs")
-    (lower lower)))
-
-
-
+  (build-system (name 'nix-go)
+                (description
+                 "Build system for Go programs")
+                (lower lower)))
--- shika/build/nix-go-build-system.scm
+++ shika/build/nix-go-build-system.scm
@@ -6,8 +6,7 @@
   #:use-module (guix build utils)
   #:use-module ((guix build gnu-build-system) #:prefix gnu:)
   #:use-module (srfi srfi-1)
-  #:export (%standard-phases
-            nix-go-build))
+  #:export (%standard-phases nix-go-build))
 
 (define* (setup-nix-go-environment #:key goos goarch #:allow-other-keys)
   (let ((tmpdir (or (getenv "TMPDIR") "/tmp")))
@@ -29,15 +28,17 @@
   (let ((vendor-dir (assoc-ref inputs "vendor")))
     (invoke "rm" "-rf" "vendor" "go.mod" "go.sum")
     (when (file-exists? (string-append vendor-dir "/go.mod"))
-      (symlink (string-append vendor-dir "/go.mod")
-               "go.mod"))
+      (symlink (string-append vendor-dir "/go.mod") "go.mod"))
     (when (file-exists? (string-append vendor-dir "/go.sum"))
-      (symlink (string-append vendor-dir "/go.sum")
-               "go.sum"))
-    (symlink (string-append vendor-dir "/vendor")
-             "vendor")))
+      (symlink (string-append vendor-dir "/go.sum") "go.sum"))
+    (symlink (string-append vendor-dir "/vendor") "vendor")))
 
-(define* (build #:key ldflags tags build-flags sub-packages #:allow-other-keys)
+(define* (build #:key
+                ldflags
+                tags
+                build-flags
+                sub-packages
+                #:allow-other-keys)
   (let ((tags (if (list? tags)
                   (list (string-append "-tags="
                                        (string-join tags ",")))
@@ -45,10 +46,11 @@
         (ldflags (list "-ldflags"
                        (if (list? ldflags)
                            (string-join (if (every (lambda (el)
-                                                     (not (string-prefix? "-buildid=" el))) ldflags)
-                                            (cons "-buildid=" ldflags)
-                                            ldflags) " ")
-                           "-buildid="))))
+                                                     (not (string-prefix?
+                                                           "-buildid=" el)))
+                                                   ldflags)
+                                            (cons "-buildid=" ldflags) ldflags)
+                                        " ") "-buildid="))))
     (for-each (lambda (pkg)
                 (apply invoke
                        `("go" "install" ,@build-flags ,@tags ,@ldflags ,pkg)))
@@ -63,10 +65,15 @@
       (mkdir-p bindir)
       (copy-recursively gobin bindir))))
 
-(define* (nix-go-build #:key inputs (phases %standard-phases)
+(define* (nix-go-build #:key
+                       inputs
+                       (phases %standard-phases)
                        #:allow-other-keys #:rest args)
   "Build the given Go package, applying all of PHASES in order."
-  (apply gnu:gnu-build #:inputs inputs #:phases phases args))
+  (apply gnu:gnu-build
+         #:inputs inputs
+         #:phases phases
+         args))
 
 (define %standard-phases
   (modify-phases gnu:%standard-phases
@@ -76,7 +83,7 @@
     (delete 'check)
     (replace 'build build)
     (replace 'install install)
-    (add-before 'unpack 'setup-nix-go-environment setup-nix-go-environment)
-    (add-after 'patch-source-shebangs 'symlink-vendor symlink-vendor)))
-
-
+    (add-before 'unpack 'setup-nix-go-environment
+      setup-nix-go-environment)
+    (add-after 'patch-source-shebangs 'symlink-vendor
+      symlink-vendor)))
