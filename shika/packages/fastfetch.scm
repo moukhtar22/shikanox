7728c6be78b0b56d96cf267438e743f66800ac40
Nikita Mitasov <me@ch4og.com>
2026-03-03 21:11:51 +0300

    packages: fastfetch: init

---
+++ shika/packages/fastfetch.scm
@@ -0,0 +1,15 @@
+;;; SPDX-FileCopyrightText: 2026 Nikita Mitasov <me@ch4og.com>
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+
+(define-module (shika packages fastfetch)
+  #:use-module (guix packages)
+  #:use-module (gnu packages admin))
+
+(define-public fastfetch-no-zfs
+  (package
+    (inherit fastfetch)
+    (name "fastfetch-no-zfs")
+    (inputs
+     (modify-inputs (package-inputs fastfetch)
+       (delete 'zfs)))))
+fastfetch-no-zfs
