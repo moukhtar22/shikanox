a7c896b512db8b33cb368672869502533cef56d2
Nikita Mitasov <me@ch4og.com>
2026-03-16 00:37:15 +0300

    utils: override: make more universal

--- shika/utils/override.scm
+++ shika/utils/override.scm
@@ -6,22 +6,29 @@
   #:use-module (guix download)
   #:use-module (guix packages)
   #:use-module (guix gexp)
-  #:export (shika-git-override))
+  #:use-module (guix deprecation)
+  #:export (shika-override
+            shika-git-override))
 
-(define* (shika-git-override base
+(define* (shika-override base
                              #:key
                              (name (package-name base))
-                             version
-                             commit
+                             (version (package-version base))
+                             (commit (string-append "v" version))
                              (url (let ((uri (origin-uri (package-source base))))
                                     (if (git-reference? uri)
                                         (git-reference-url uri)
                                         uri)))
                              hash
+                             (inputs (package-inputs base))
+                             (native-inputs (package-native-inputs base))
                              (home-page (package-home-page base)))
   (package
     (inherit base)
-    (version (git-version version "0" commit))
+    (version
+     (if (string-prefix? "v" commit)
+         version
+         (git-version version "0" commit)))
     (source
      (origin
        (method git-fetch)
@@ -31,4 +38,8 @@
        (file-name (git-file-name name version))
        (sha256
         (base32 hash))))
+    (inputs inputs)
+    (native-inputs native-inputs)
     (home-page home-page)))
+
+(define-deprecated/alias shika-git-override shika-override)
